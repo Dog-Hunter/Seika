37c8035b47bd9ceda8288cec383693e4389cdcb2
Vasiliy ZHigulev <yzhestvsky@mail.ru>
2023-04-07 21:15:31 +0500

    Added playlist mode

--- cogs/music.py
+++ cogs/music.py
@@ -13,7 +13,7 @@ class Music(commands.Cog):
         self.player = player
         self.vc = None
         self.is_paused = True
-
+    
     @commands.command(name='music')
     async def music_cmd(self, ctx, *args):
         if ctx.author.voice is None:
@@ -36,10 +36,31 @@ class Music(commands.Cog):
     async def add_cmd(self, ctx, *args):
         track = await self.player.search_track(' '.join(args))
         self.player.queue.append(track)
-        track_info = await self.player.get_track_info(self.player.queue[-1])
+        track_info = await self.player.get_track_info(track)
         await ctx.reply(f'Ok, I\'ll add to list: {track_info}')
 
-    @commands.command(name='pause', description="pauses music")
+    @commands.command(name='playlist')
+    async def playlist_cmd(self, ctx, *args):
+        if ctx.voice_client is None:
+            channel = ctx.message.author.voice.channel
+            vc = await channel.connect()
+        else:
+            vc = ctx.voice_client
+
+        if None in args:
+            await self.player.playlist()
+        else:
+            await self.player.playlist(int(''.join(args)))
+        while len(self.player.queue) > 0:
+            await self.player.play()
+            await ctx.send(f'Playing {self.player.current_track_info}')
+            vc.play(discord.FFmpegPCMAudio(executable=r"FF/ffmpeg.exe", source=r'FF/track.mp3'))
+            while vc.is_playing() or vc.is_paused():
+                await asyncio.sleep(1)
+        vc.stop()
+        await vc.disconnect()
+
+    @commands.command(name='pause')
     async def pause_cmd(self, ctx,):
         if ctx.voice_client.is_paused():
             return
@@ -51,6 +72,11 @@ class Music(commands.Cog):
         if ctx.voice_client.is_playing():
             return
         ctx.voice_client.resume()
+
+    @commands.command(name='clear')
+    async def clear_cmd(self, ctx,):
+        self.player.queue.clear()
+        await ctx.reply('OK, the concert is over for today. Finish your nao and go home.')
 
 async def setup(client):
     c = await (ClientAsync(YANDEX_MUSIC_TOKEN)).init()
--- modules/player.py
+++ modules/player.py
@@ -1,12 +1,11 @@
 import yandex_music as ym
-
 import config
 
 config.YANDEX_MUSIC_TOKEN
 
 
 class Player:
-    def __init__(self, client):
+    def __init__(self, client: ym.ClientAsync):
         self.client = client
         self.current_track = None
         self.current_track_info = None
@@ -22,11 +21,14 @@ class Player:
 
     async def get_track_info(self, track):
         track_name = track.title
-        if len(self.current_track.artists) > 1:
-            artist = ', '.join([i.name for i in self.current_track.artists])
-        else:
-            artist = self.current_track.artists[0].name
-        album = self.current_track.albums[0].title
+        try:
+            if len(track.artists) > 1:
+                artist = ', '.join([i.name for i in track.artists])
+            else:
+                artist = track.artists[0].name
+        except:
+            artist = 'I don\'t give a fuck who are they'
+        album = track.albums[0].title
         return f'{artist} - {track_name} ({album})'
 
     async def play(self):
@@ -38,5 +40,7 @@ class Player:
         self.current_track = self.queue.pop(0)
         self.download_track()
 
-    async def clear_queue(self):
-        self.queue.clear()
+    async def playlist(self, playlist_id=3):
+        tracks = (await self.client.users_playlists(playlist_id)).tracks
+        tracks = [i.track for i in tracks]
+        self.queue = tracks
